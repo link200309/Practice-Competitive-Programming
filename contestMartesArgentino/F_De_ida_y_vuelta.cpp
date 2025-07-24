cf7e9c8c40d8c3464ae498499bd2b815d9118d32
link200309 <jonhh.0023@gmail.com>
2025-07-24 18:32:00 -0400

    feat(contestMartesArgentino): add solución F_De_ida_y_vuelta

---
+++ contestMartesArgentino/F_De_ida_y_vuelta.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+void solve(){
+    int c;
+
+    while (cin >> c){
+        string p; cin >> p;
+        vector<vector<char>> mp(p.size()/c);
+
+        for(int i = 0; i < mp.size(); i++){
+            for(int j = 0; j < mp[i].size(); j++){
+                mp[i][j] = 
+            }
+        }
+
+
+
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int T = 1;
+    while(T--){solve();}
+    return 0;
+}
